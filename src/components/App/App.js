07534ec681b96a52d834b01276fdab5d7e032b36
Christopher Culbreath <christopher@moltenlavapit.com>
2020-09-19 12:26:29 -0700

    2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -22,8 +22,8 @@ class App extends Component{
       <header className="App-header">
         Turbo-Matcher
       </header>
-        <OptionsPanel />
-        <Board />
+        <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} />
+        <Board numTiles={this.state.numTiles} tiles={this.state.tiles} />
       }
     </div>
   );
